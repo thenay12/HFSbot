7517c8191110d662d1f189daba8f2513d1d4f3c6
thenay12 <81709656+thenay12@users.noreply.github.com>
2021-04-01 12:54:15 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,78 @@
+const help = (prefix) => {
+	return `┌──「 *HFS BOT WA* 」
+│
+├「 OWNER STATUS 」
+│
+├◪ *NAME: Rouf X Adinda Hafis*
+├◪ *WA: 16028848870*
+├◪ *YT: SkyLark*
+│
+├──「 *TENTANG BOT* 」
+│
+├◪ ${prefix}info
+├◪ ${prefix}owner
+├◪ ${prefix}donasi
+├◪ ${prefix}blocklist
+│
+├──「 *SIMPLE MENU* 」
+│
+├◪ ${prefix}sticker
+├◪ ${prefix}sticker nobg
+├◪ ${prefix}toimg
+├◪ ${prefix}ttp
+├◪ ${prefix}tts
+├◪ ${prefix}nulis
+│
+├──「 *DOWNLOADER* 」
+│
+├◪ ${prefix}tiktod
+├◪ ${prefix}ytmp3
+│
+├──「 *MEME MENU* 」
+│
+├◪ ${prefix}meme
+├◪ ${prefix}memeindo
+│
+├──「 *GRUP MENU* 」
+│
+├◪ ${prefix}admin
+├◪ ${prefix}welcome [1/0]
+├◪ ${prefix}add
+├◪ ${prefix}kick
+├◪ ${prefix}promote
+├◪ ${prefix}demote
+├◪ ${prefix}tagall
+├◪ ${prefix}tagall2
+├◪ ${prefix}tagall3
+├◪ ${prefix}linkgrup
+├◪ ${prefix}leave
+├◪ ${prefix}simih [1/0]
+│
+├──「 *18+ MENU* 」
+│
+├◪ ${prefix}loli
+├◪ ${prefix}nsfwloli
+│
+├──「 *OTHER MENU* 」
+│
+├◪ ${prefix}quotes
+├◪ ${prefix}ssweb
+├◪ ${prefix}simi
+├◪ ${prefix}ocr
+├◪ ${prefix}wait
+├◪ ${prefix}tiktokstalk
+├◪ ${prefix}hilih
+├◪ ${prefix}ytstalk
+│
+├──「 *OWNER MENU* 」
+│
+├◪ ${prefix}setprefix
+├◪ ${prefix}bc
+├◪ ${prefix}clearall
+├◪ ${prefix}clone
+│
+└──「 *H F S  B O T* 」
+`
+}
+
+exports.help = help
---
+++ src/help.js.bak
@@ -0,0 +1,78 @@
+const help = (prefix) => {
+	return `┌──「 *FDL BOT WA* 」
+│
+├「 OWNER STATUS 」
+│
+├◪ *NAME: FDL*
+├◪ *WA: 628990542731*
+├◪ *YT: FDL GANZ*
+│
+├──「 *TENTANG BOT* 」
+│
+├◪ ${prefix}info
+├◪ ${prefix}owner
+├◪ ${prefix}donasi
+├◪ ${prefix}blocklist
+│
+├──「 *SIMPLE MENU* 」
+│
+├◪ ${prefix}sticker
+├◪ ${prefix}sticker nobg
+├◪ ${prefix}toimg
+├◪ ${prefix}ttp
+├◪ ${prefix}tts
+├◪ ${prefix}nulis
+│
+├──「 *DOWNLOADER* 」
+│
+├◪ ${prefix}tiktod
+├◪ ${prefix}ytmp3
+│
+├──「 *MEME MENU* 」
+│
+├◪ ${prefix}meme
+├◪ ${prefix}memeindo
+│
+├──「 *GRUP MENU* 」
+│
+├◪ ${prefix}admin
+├◪ ${prefix}welcome [1/0]
+├◪ ${prefix}add
+├◪ ${prefix}kick
+├◪ ${prefix}promote
+├◪ ${prefix}demote
+├◪ ${prefix}tagall
+├◪ ${prefix}tagall2
+├◪ ${prefix}tagall3
+├◪ ${prefix}linkgrup
+├◪ ${prefix}leave
+├◪ ${prefix}simih [1/0]
+│
+├──「 *18+ MENU* 」
+│
+├◪ ${prefix}loli
+├◪ ${prefix}nsfwloli
+│
+├──「 *OTHER MENU* 」
+│
+├◪ ${prefix}quotes
+├◪ ${prefix}ssweb
+├◪ ${prefix}simi
+├◪ ${prefix}ocr
+├◪ ${prefix}wait
+├◪ ${prefix}tiktokstalk
+├◪ ${prefix}hilih
+├◪ ${prefix}ytstalk
+│
+├──「 *OWNER MENU* 」
+│
+├◪ ${prefix}setprefix
+├◪ ${prefix}bc
+├◪ ${prefix}clearall
+├◪ ${prefix}clone
+│
+└──「 *FDL BOT* 」
+`
+}
+
+exports.help = help
